044cb59440b1361adccbd6080fca033e48242a53
MarianaCoceban <mariana_coceban@yahoo.com>
2024-03-20 16:34:11 +0200

    first

---
+++ index.js
@@ -0,0 +1,47 @@
+//ex.1
+const num = 8
+const str = 'string'
+const bool = true
+const unde = undefined
+const testNull = null
+const testBigInt = BigInt(7889)
+const symb = Symbol('')
+
+const obj = {}
+
+//console.log(typeof unde)
+
+//ex.2
+let num1 = 8
+let num2 = 7
+
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num1 * num2)
+console.log(num1 / num2)
+
+num1++
+console.log(num1)
+num1--
+console.log(num1)
+
+console.log(++num1)
+console.log(--num1)
+
+console.log(num1 += num2)
+console.log(num1 -= num2)
+
+console.log(num1 ** num2)
+
+const bool1 = true
+console.log(!bool1)
+
+console.log(num1 > num2)
+console.log(num1 < num2)
+console.log(num1 >= num2)
+console.log(num1 <= num2)
+
+console.log(num1 == num2)
+console.log(num1 === num2)
+console.log(num1 !== num2)
+console.log(num1 != num2)
